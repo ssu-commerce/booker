65bebeb68e7641d2b76189d2478a2fe36d17fba4
always0ne <always0ne.dev@gmail.com>
2022-07-09 13:17:30 +0900

    gh-pages 연동

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import React from "react";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
